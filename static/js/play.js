08c1a7a48f2eaa88085de3b6fa784814cef9988f
Sam Bryant <swb42@txstate.edu>
2023-04-07 03:11:57 -0500

    put abby's view on main play page

--- static/js/play.js
+++ static/js/play.js
@@ -9,6 +9,21 @@
 // server sends game state,
 // repeat...
 
+//CONSTANTS
+
+const OPP_CLASS = "row opponent"
+const PLYR_CLASS = "row player"
+
+const ROUND_CLASS = "col-sm round"
+
+
+
+function generateCard(cardkey) { // example: A-3 (applewood 3)
+
+}
+
+
+/*
     $(document).ready(function () {
       console.log(io.version)
       var socket = io.connect('http://127.0.0.1:3000');
@@ -54,38 +69,7 @@
       window.location.href = 'gameover'
     });
 
-      /*socket.on('result', function(data) {
-                $('#player1_card').text(data.player1_card);
-                $('#player2_card').text(data.player2_card);
-                $('#result').text(data.winner);
-            });
-
-      socket.on('played', function(data) {
-          pickedCard = data["cardValue"]
-          playedCards = $('#player2played_cards').text()
-          playedCards += " " + pickedCard
-          $('#player2played_cards').text(playedCards)
-
-          currentHand = $('#player2_card').text()
-          $('#player2_card').text(currentHand.replace(pickedCard + " ", ""))
-
-          $('#winners').text(data['result'])
-        })
-
-
-      $('#send-button').click(function() {
-                pickedCard = $('#cardToPlay').val()
-                playedCards = $('#player1played_cards').text()
-                playedCards += " " + pickedCard
-                $('#player1played_cards').text(playedCards)
-                let gid = window.location.pathname.slice(6)
-                let sid = $('#sid').text()
-                socket.emit('playedCard', { gid: gid, sid: sid, card: pickedCard});
-		            $('#cardToPlay').val('');
-
-                currentHand = $('#player1_card').text()
-                $('#player1_card').text(currentHand.replace(pickedCard + " ", ""))
-
-            });
-       */ 
     });
+*/
+
+    
