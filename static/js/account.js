eda8c05c135957c8068837e24532f7b924ecee27
Abigail Barrientos <wsx1@txstate.edu>
2023-04-22 11:25:37 -0500

    Update user profile photo
    This code allows you to go to your account page and upload a user profile there. Will add server functionality.

--- static/js/account.js
+++ static/js/account.js
@@ -1,23 +1,30 @@
-const editIcon = document.querySelector('.edit-icon');
-const modal = document.querySelector('.modal');
-const images = document.querySelectorAll('.image');
-const profilePicture = document.querySelector('.profile-picture');
 
-editIcon.addEventListener('click', () => {
-  modal.style.display = 'block';
-});
+  const imgDiv = document.querySelector('.user_img');
+  const img = document.querySelector('#photo');
+  const file = document.querySelector('#file');
+  const upload = document.querySelector('#upload');
 
-images.forEach((image) => {
-  image.addEventListener('click', () => {
-    profilePicture.src = image.src;
-    modal.style.display = 'none';
+  //if user hovers on img div, show upload
+  imgDiv.addEventListener('mouseenter', function() {
+    upload.style.display = "block";
   });
-});
 
-window.addEventListener('click', (event) => {
-  if (event.target == modal) {
-    modal.style.display = 'none';
-  }
-});
+  //if we hover out of imgDiv
+  imgDiv.addEventListener('mouseleave', function() {
+    upload.style.display = "none";
+  });
 
+  file.addEventListener('change', function(){
+    const chooseFile = this.files[0];
 
+    if(chooseFile) {
+
+      const reader = new FileReader();
+
+      reader.addEventListener('load', function(){
+        img.setAttribute('src', reader.result);
+      });
+
+      reader.readAsDataURL(chooseFile);
+    }
+  });
