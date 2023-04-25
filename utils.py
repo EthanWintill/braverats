82a440c40f54884e16cc24d6bf2d547b3d00be52
EW <96669967+bigbrrrr@users.noreply.github.com>
2023-04-25 02:59:40 -0500

    basic leaderboard with button
    Still need to add CSS to it

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from models import Users
+from models import Users, History
 
 class Authentic():
     def gen_usr_token(uid, pswd_hash):
@@ -19,4 +19,27 @@ def test():
     hash = 'sha4208539y4395340285u843rhtwreg9g943w89545==='
     token = Authentic.gen_usr_token(uid,hash)
     print(Authentic.token_to_id(token))
+
+
+def getLeaderboard():
+    games = History.readAll()
+    users = Users.getAllUsers()
+    board = []
+    for user in users:
+        username = user['username']
+        id = user['id']
+        wins = 0
+        losses = 0
+        for game in games:
+            if game['appleid'] == id and game['applescore']==4 or game['yargid']==id and game['yargscore']==4:
+                wins+=1
+            elif game['appleid'] == id and game['applescore']!=4 or game['yargid']==id and game['yargscore']!=4:
+                losses+=1
+        games_played = wins+losses
+        raw_ratio = wins/(wins+losses) if losses>0 or wins>0 else -1
+        ratio = int(raw_ratio*100)
+        board.append([username,id,games_played,wins,losses,ratio])
+
+    return sorted(board, key=lambda x: x[5], reverse=True)
+
 
