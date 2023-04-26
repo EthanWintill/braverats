dad9b2840e5ccba1896ba6091913ff8fce5fe773
Sam Bryant <swb42@txstate.edu>
2023-04-26 00:46:31 -0500

    delete games after 20 minutes

--- games.py
+++ games.py
@@ -2,6 +2,7 @@ from braverats import Game, Bot
 import random
 import string
 import hashlib
+import threading
 
 games = {}
 
@@ -14,14 +15,22 @@ CHARACTERS = (
 def generate_unique_key():
     return ''.join(random.sample(CHARACTERS, 15))
 
+def deleteGame(gId):
+    global games
+    if gId in games:
+        del games[gId]
+        print("get that game outa here")
+
 def createNewGame(oldGID = None, isOneplayer=False):
     global games
-    if len(games) > 4:
-        games = {}
+
     if oldGID is None: #check if new match instead of rematch
         gId = generate_unique_key()
         ng = Game(gId, isOneplayer=True) if isOneplayer else Game(gId)
         games[gId] = ng
+        #set timer
+        timer = threading.Timer(20 * 60, deleteGame, args=[gId])
+        timer.start()
         return gId
 
     #if we're at this point we know it's rematch
@@ -31,6 +40,9 @@ def createNewGame(oldGID = None, isOneplayer=False):
     except:
         ng = Game(gId, isOneplayer=True) if isOneplayer else Game(gId) #create new game if client was the first to click rematch
         games[gId] = ng
+        #timer
+        timer = threading.Timer(20 * 60, deleteGame, args=[gId])
+        timer.start()
     return gId
 
 def createOnePlayerGame(oldGID = None): #literally exactly the same as before except for the game constuctor
